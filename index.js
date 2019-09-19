017004173e03092b8f8f346367ddfba1fb90d153
pradiptaagus <igedeaguspradipta@gmail.com>
2019-09-19 11:05:40 +0800

    first

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require('path');				// use path module
+const fs = require('fs');
+const express = require('express');			// Use express module
+const bodyParser = require('body-parser');	// Use bodyParser middleware
+const hbs = require('./lib/hbs_helper');					// Use hbs view engine
+const app = express();
+
+// hbs partial file
+hbs.registerPartial('navbar', fs.readFileSync(__dirname + '/views/layouts/navbar.hbs', 'utf8'));
+hbs.registerPartial('sidebar', fs.readFileSync(__dirname + '/views/layouts/sidebar.hbs', 'utf8'));
+
+// Set view files
+app.set('views', path.join(__dirname, 'views'));
+
+// Set view engine
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+// Set public folder as static folder for static files
+app.use('/assets', express.static(__dirname + '/public'));
+
+// Routes
+app.use(require('./route/product'));
+
+
+app.listen(3000, () => {
+	console.log('Server is running at port 3000');
+});
---
+++ route/product.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const router = express.Router();
+const conn = require('../lib/database');
+
+
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ' + Date.now());
+    next()
+});
+
+router.get('/product', (req, res) => {
+	let sql = "SELECT productID, name, price FROM products";
+	let path = req.path.split('/');
+	conn.query(sql, (err, results) => {
+		if (err) throw err;
+		res.render('products/product_view', {
+			results: results,
+			page: path[1]
+		});
+	});
+});
+
+router.get('/product/add', (req, res) => {
+	let path = req.path.split('/');
+	res.render('products/add_product', {
+		page: path[1]
+	});
+});
+
+router.post('/product', (req, res) => {
+	let productID = new Date().getTime();
+	let data = {
+		productID: productID,
+		name: req.body.name, 
+		price: req.body.price
+	};
+	let sql = "INSERT INTO products SET ?";
+	conn.query(sql, data, (err, results) => {
+		if (err) throw err;
+		res.redirect('/product');
+	});
+});
+
+router.get('/product/edit/:id', (req, res) => {
+	let path = req.path.split('/');
+	let sql = "SELECT productID, name, price FROM products WHERE productID='"+ req.params.id +"'";
+	conn.query(sql, (err, results) => {
+		if (err) throw err;
+		res.render('products/edit_product', {
+			data: results,
+			page: path[1]
+		});
+	});
+});
+
+router.post('/product/:id', (req, res) => {
+	let sql = "UPDATE products SET name='" + req.body.name + "', price='"+ req.body.price +"' WHERE productID='"+ req.params.id +"' ";
+	conn.query(sql, (err, results) => {
+		if (err) throw err;
+		res.redirect('/product');
+	});
+});
+
+router.get('/product/delete/:id', (req, res) => {
+	let sql = "DELETE FROM products WHERE productID='"+req.params.id+"'";
+	conn.query(sql, (err, results) => {
+		if (err) throw err;
+		res.redirect('/product');
+	});
+});
+
+module.exports = router;
